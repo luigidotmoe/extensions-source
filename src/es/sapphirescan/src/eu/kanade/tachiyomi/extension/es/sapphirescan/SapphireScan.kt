6899d44937eb302aee5b18dcc32ddb64eb80ecac
luigidotmoe <19366824+luigidotmoe@users.noreply.github.com>
2026-05-15 19:11:38 +0200

    SapphireScan (ES): Migrate from Madara to ZeistManga

--- src/es/sapphirescan/src/eu/kanade/tachiyomi/extension/es/sapphirescan/SapphireScan.kt
+++ src/es/sapphirescan/src/eu/kanade/tachiyomi/extension/es/sapphirescan/SapphireScan.kt
@@ -1,44 +1,18 @@
 package eu.kanade.tachiyomi.extension.es.sapphirescan
 
-import eu.kanade.tachiyomi.multisrc.madara.Madara
+import eu.kanade.tachiyomi.multisrc.zeistmanga.ZeistManga
 import eu.kanade.tachiyomi.network.interceptor.rateLimitHost
-import eu.kanade.tachiyomi.source.model.Page
-import eu.kanade.tachiyomi.source.model.SChapter
 import okhttp3.HttpUrl.Companion.toHttpUrl
-import org.jsoup.nodes.Document
-import org.jsoup.nodes.Element
-import java.text.SimpleDateFormat
-import java.util.Locale
 
 class SapphireScan :
-    Madara(
+    ZeistManga(
         "SapphireScan",
-        "https://sapphirescan.com",
+        "https://www.sapphirescan.com",
         "es",
-        SimpleDateFormat("MMMM dd, yyyy", Locale("es")),
     ) {
     override val client = super.client.newBuilder()
         .rateLimitHost(baseUrl.toHttpUrl(), 3)
         .build()
 
-    override val useNewChapterEndpoint = true
-    override val useLoadMoreRequest = LoadMoreStrategy.Always
-
-    override fun chapterFromElement(element: Element): SChapter = super.chapterFromElement(element).apply {
-        if (element.select("span.required-login").isNotEmpty()) {
-            name = "🔒 $name"
-        }
-    }
-
-    override fun pageListParse(document: Document): List<Page> {
-        val pageList = super.pageListParse(document)
-
-        if (
-            pageList.isEmpty() &&
-            document.select(".content-blocked, .login-required").isNotEmpty()
-        ) {
-            throw Exception("Inicie sesión en WebView para ver este capítulo")
-        }
-        return pageList
-    }
+    override val pageListSelector = "div.check-box"
 }
